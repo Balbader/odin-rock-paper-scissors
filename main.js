0c6e7d072037d9cb0888fb43999248c40c0daa94
Balbader <15971200+Balbader@users.noreply.github.com>
2021-11-25 13:22:49 +0100

    main.js

--- main.js
+++ main.js
@@ -5,30 +5,40 @@ const computerPlay = () => {
   return randomAnswer;
 };
 
-const playerSelection = 1;
-const computerSelection = computerPlay();
-
 //Function that plays a single round of the game
-const playRound = (playerSelection, computerSelection) => {
+const playRound = () => {
+  let playerSelection = prompt(
+    "Enter Selection: 0.Rock - 1.Paper - 2.Scissors"
+  );
+  const computerSelection = computerPlay();
   const selection = ["Rock", "Paper", "Scissors"];
-  if (playerSelection > computerSelection) {
-    console.log(`computerPlay: ${selection[computerSelection]}`);
-    console.log(`playerSelection: ${selection[playerSelection]}`);
-    console.log(
-      `You Win! ${selection[playerSelection]} beats ${selection[computerSelection]}`
-    );
-    return 1;
-  } else if (playerSelection < computerSelection) {
-    console.log(`computerPlay: ${selection[computerSelection]}`);
-    console.log(`playerSelection: ${selection[playerSelection]}`);
-    console.log(
-      `You Loose! ${selection[computerSelection]} beats ${selection[playerSelection]}`
-    );
-    return 0;
-  } else {
-    console.log("Try again! You both picked the same answer.");
-    return "=";
+  if (playerSelection > 2) {
+    while (playerSelection > 2) {
+      playerSelection = prompt(
+        `Please Select a number between 0 and 2.\n0.Rock - 1.Paper - 2.Scissors`
+      );
+    }
+  }
+  if (playerSelection >= 0 && playerSelection <= 2) {
+    if (playerSelection > computerSelection) {
+      console.log(`Computer Selection: ${selection[computerSelection]}`);
+      console.log(`Player Selection: ${selection[playerSelection]}`);
+      console.log(
+        `You Win! ${selection[playerSelection]} beats ${selection[computerSelection]}`
+      );
+      return 1;
+    } else if (playerSelection < computerSelection) {
+      console.log(`Computer Selection: ${selection[computerSelection]}`);
+      console.log(`Player Selection: ${selection[playerSelection]}`);
+      console.log(
+        `You Loose! ${selection[computerSelection]} beats ${selection[playerSelection]}`
+      );
+      return 0;
+    } else {
+      console.log("Try again! You both picked the same answer.");
+      return "=";
+    }
   }
 };
 
-playRound(playerSelection, computerSelection);
+const game = () => {};
