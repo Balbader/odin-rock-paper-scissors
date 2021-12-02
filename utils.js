484bbf3d421361d0cfd64d1f69e2f2c5022c53c5
Balbader <15971200+Balbader@users.noreply.github.com>
2021-12-02 13:35:53 +0100

    file deletion after moving function to app.js

--- utils.js
+++
@@ -1,7 +0,0 @@
-const computerPlay = () => {
-	const array = ['Rock', 'Paper', 'Scissors'];
-	const randomAnswer = Math.floor(Math.random() * array.length);
-	return randomAnswer;
-};
-
-export {computerPlay};
